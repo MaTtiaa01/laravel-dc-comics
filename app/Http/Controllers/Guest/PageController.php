5395832236b32093da5a6685f79efb936420a46e
MaTtiaa01 <mattia2001@live.it>
2022-12-22 18:15:28 +0100

    print in welcome page

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Comic;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $comics = Comic::orderByDesc('id')->get();
+        return view('welcome', compact('comics'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,28 @@
 
 @section('content')
 
+<div class="conatiner py-5">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        @forelse($comics as $comic)
+        <div class="col text-center">
+            <div class="card">
+                <div class="card-header">
+                    {{$comic->title}}
+                </div>
+                <div class="card-body">
+                    <img src="{{$comic->thumb}}" alt="">
+                    <div class="details">
+                        <div class="price my-4">Price: {{$comic->price}}$</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @empty
+        <div class="col-12">
+            No comics available yet!
+        </div>
+        @endforelse
+    </div>
+</div>
 
 @endsection
