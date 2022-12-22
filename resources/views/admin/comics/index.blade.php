156cd0a9e79c2b6ecb129e86c21e0cd714d4d911
MaTtiaa01 <mattia2001@live.it>
2022-12-22 17:58:58 +0100

    try to add destroy function

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -5,24 +5,46 @@
 <section class="user_comics py-5">
     <div class="container">
         <h2>Comics List</h2>
-        <div class="row g-4">
-            @forelse($comics as $comic)
-            <div class="col-12">
-                <div class="card d-flex justify-content-between">
-                    <h6>{{$comic->title}}</h6>
-                    <div class="price">{{$comic->price}}</div>
-                    <div class="thumb">
-                        <img width="80" src="{{$comic->thumb}}" alt="">
-                    </div>
-                </div>
-            </div>
-            @empty
-            <div class="col-12">
-                Ops! no comics available yet!
-                Go Add some new comics!
-            </div>
-            @endforelse
+
+        <div class="table-responsive">
+            <table class="table table-primary">
+                <thead>
+                    <tr>
+                        <th scope="col"> id</th>
+                        <th scope="col">title </th>
+                        <th scope="col"> thumb</th>
+                        <th scope="col"> price</th>
+                        <th scope="col"> series</th>
+                        <th scope="col"> sale_date</th>
+                        <th scope="col"> type</th>
+                        <th scope="col"> actions</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($comics as $comic)
+                    <tr class="">
+                        <td scope="row">{{$comic->id}}</td>
+                        <td>{{$comic->title}}</td>
+                        <td>{{$comic->thumb}}</td>
+                        <td>{{$comic->price}}</td>
+                        <td>{{$comic->series}}</td>
+                        <td>{{$comic->sale_date}}</td>
+                        <td>{{$comic->type}}</td>
+                        <td>
+                            <a class="btn btn-primary" href="#">view</a>
+                            <a class="btn btn-primary" href="{{route('comics.edit',$comic->id)}}">edit</a>
+                            <a class="btn btn-danger" href="{{route('comics.destroy',$comic->id)}}">delete</a>
+
+                        </td>
+                    </tr>
+                    @empty
+
+                    @endforelse
+                </tbody>
+            </table>
         </div>
+
     </div>
 
 </section>
