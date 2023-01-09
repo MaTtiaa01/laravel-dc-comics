e7933a74a4baf95e0863a0305283e2e4af9ae1bf
MaTtiaa01 <mattia2001@live.it>
2023-01-09 15:30:55 +0100

    add old() value in create form

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -19,30 +19,30 @@
 
         <div class="mb-3">
             <label for="title" class="form-label">title</label>
-            <input type="text" name="title" id="title" class="form-control" placeholder="Title" aria-describedby="helpId">
+            <input type="text" name="title" id="title" class="form-control" placeholder="Title" aria-describedby="helpId" value="{{old('title')}}">
         </div>
         <div class="mb-3">
-            <textarea name="description" id="description" cols="30" rows="10"></textarea>
+            <textarea name="description" id="description" cols="30" rows="10">{{old('description')}}</textarea>
         </div>
         <div class="mb-3">
             <label for="thumb" class="form-label">thumb</label>
-            <input type="text" name="thumb" id="thumb" class="form-control" placeholder="thumb" aria-describedby="helpId">
+            <input type="text" name="thumb" id="thumb" class="form-control" placeholder="thumb" aria-describedby="helpId" value="{{old('thumb')}}">
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">price</label>
-            <input type="text" name="price" id="price" class="form-control" placeholder="price" aria-describedby="helpId">
+            <input type="text" name="price" id="price" class="form-control" placeholder="price" aria-describedby="helpId" value="{{old('price')}}">
         </div>
         <div class="mb-3">
             <label for="series" class="form-label">series</label>
-            <input type="text" name="series" id="series" class="form-control" placeholder="series" aria-describedby="helpId">
+            <input type="text" name="series" id="series" class="form-control" placeholder="series" aria-describedby="helpId" value="{{old('series')}}">
         </div>
         <div class="mb-3">
             <label for="sale_date" class="form-label">sale_date</label>
-            <input type="text" name="sale_date" id="sale_date" class="form-control" placeholder="sale_date" aria-describedby="helpId">
+            <input type="text" name="sale_date" id="sale_date" class="form-control" placeholder="sale_date" aria-describedby="helpId" value="{{old('sale_date')}}">
         </div>
         <div class="mb-3">
             <label for="type" class="form-label">type</label>
-            <input type="text" name="type" id="type" class="form-control" placeholder="type" aria-describedby="helpId">
+            <input type="text" name="type" id="type" class="form-control" placeholder="type" aria-describedby="helpId" value="{{old('type')}}">
         </div>
         <button class="btn btn-primary" type="submit">Add new comic</button>
     </form>
